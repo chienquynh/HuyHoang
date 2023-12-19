bfeb24a08da1824da1b6ee6aa38c9270b97d60dd
chien111 <124616791+chienquynh@users.noreply.github.com>
2023-12-19 22:38:11 +0700

    nhap mon ai

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Map from './components/map';
+
+function App() {
+  return (
+    <div>
+      <Map/>
+    </div>
+  );
+}
+
+export default App;
